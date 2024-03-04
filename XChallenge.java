f1c5939c327c285544fdf307d29426ca96aaafdc
Jacob Morello <alternationgd57@gmail.com>
2024-03-04 11:26:02 -0500

    Added X Challenge

--- XChallenge.java
+++ XChallenge.java
@@ -1,28 +1,27 @@
 import java.awt.Color;
 
-public class StairCaseChallenge extends Challenge {
+public class XChallenge extends Challenge {
     public void start() {
         TetriminoNode[][] board = MyGame.board.board;
+        int node1 = 0;
+        int node2 = board[0].length - 1;
 
-        int count = 1;
-        int direction = 1;
-
-        for (int r = 3; r < board.length; r++) {
+        for (int r = board.length / 2; r < board.length; r++) {
             for (int c = 0; c < board[r].length; c++) {
-                if (c < count) {
+                if (c == node1 || c == node2) {
                     board[r][c] = new TetriminoNode(Color.DARK_GRAY, r, c, -1);
                     board[r][c].updateID();
-                } else break;
+                }
             }
 
-            count += direction;
-            if (count == board[r].length - 1 || count == 1) direction = -direction;
+            node1++;
+            node2--;
         }
     }
 
     public void end() {
         MyGame.status.addMessage("You Win!", 3000);
-        MyGame.account.addExp((int)(Math.random() * 100) + 100);
+        MyGame.account.addExp((int)(Math.random() * 200) + 100);
         MyGame.exitToMenu();
     }
 }
